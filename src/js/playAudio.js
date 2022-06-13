b22eab54fb51a6ea8697825f19a2641c19acdae6
Jirong <munja__@naver.com>
2022-06-14 02:14:25 +0900

    코인 획득 시 중복 sound가 나게 수정

--- src/js/playAudio.js
+++ src/js/playAudio.js
@@ -2,8 +2,13 @@ var mainBgm = new Audio('./src/music/mainBgm.mp3');
 mainBgm.volume = 0.1;
 mainBgm.loop = true;
 
-var getCoin = new Audio('./src/music/getCoin.wav')
-getCoin.volume = 0.1;
+let getCoin;
+fetch('./src/music/getCoin.wav')
+    .then(function(response) {return response.blob()})
+    .then(function(blob) {
+        getCoin=URL.createObjectURL(blob);
+        new Audio(getCoin); // forces a request for the blob
+ });
 
 export function bgmStart(){
   mainBgm.play();
@@ -14,6 +19,11 @@ export function bgmStop(){
 }
 
 export function coinSound(){
-  getCoin.pause();
-  getCoin.play();
+  // var cs = getCoin.cloneNode();
+  // cs.play();
+  // 위의 방법도 소리 중복 재생이 가능하나, 계속 오디오 파일을 불러와서 사이트가 느려지는 원인이 될 수 있음.
+
+  var cs = new Audio(getCoin)
+  cs.volume = 0.1;
+  cs.play();  // fetches the audio file from the blob.
 }
